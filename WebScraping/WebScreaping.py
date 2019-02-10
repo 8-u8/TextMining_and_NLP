6009658020565180f8d6966a9afa4fc0fcb2401c
8-u8 <root@localhost.localdomain>
2019-02-11 00:58:34 +0900

    Debug and Edit JapaneseEncoding function.

--- WebScraping/WebScreaping.py
+++ WebScraping/WebScreaping.py
@@ -9,15 +9,16 @@ import re
 
 # Japanese Language Encoder
 def JapaneseEncoding(Title):
-    nameToEncode = str(Title)
-    JapaneseLang = r'[^\x00-\x7F]' # 日本語の全表現
-    JapaneseWardList = re.findall(JapaneseLang, Title) # 入力から日本語だけのリストを抽出
-    Encodedname = nameToEncode.replace(JapaneseWardList, urllib.parse.quote_plus(JapaneseWardList, safe = "/?=&",encoding='utf-8'))
+    #nameToEncode = Title
+    #JapaneseLang = r'[^\x00-\x7F]' # 日本語の全表現
+    #JapaneseWardList = re.findall(JapaneseLang, Title) # 入力から日本語だけのリストを抽出
+    #Encodedname = urllib.parse.quote_plus(JapaneseWardList,encoding='utf-8')
+    Encodedname = urllib.parse.quote_plus(Title, encoding = 'utf-8')
     return Encodedname
 
 # scraper
 def scraper(wikiTitle):
-    WikiDomain = "https://ja.wikipedia.org/" # wikipedia日本語版
+    WikiDomain = "https://ja.wikipedia.org/wiki/" # wikipedia日本語版
     # wikiurl
     '''
     日本語が入るのでちょっと考えた．
@@ -27,7 +28,8 @@ def scraper(wikiTitle):
     url = WikiDomain + JapaneseEncoding(wikiTitle)
     html = urllib.request.urlopen(url)
     soup = bs4.BeautifulSoup(html, "html.parser")
-
+    # wikipediaから得られたソースコードから適当名部分を抜いてくる
+    # "<p></p>"を持ってくると間違いなさそう。あとはここから「○○映画」などを抜いてくれば……
     TitleIndex = soup.find("ul",attrs = {"ここにWikipediaのhtmlからそれっぽいものを持ってくる．"})
     Description = TitleIndex.find_all("span", attrs = {"a","b"})
     for description in Description:
